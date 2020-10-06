d6122d62c4e098054c60332bac2f2d768fbb8e68
xeon82 <xeon82@gmail.com>
2020-10-05 20:19:45 -0400

    Update2chevalet mouton

---
+++ devoirCheval.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project = "cr460-291623"
+  credentials = "account.json"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "Apache2" {
+  name         = "canard"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "fedora-coreos-cloud/fedora-coreos-stable"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
