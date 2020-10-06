84985052cb3d5d3ed8a6831d9648b4891c90fdbe
xeon82 <xeon82@gmail.com>
2020-10-05 20:33:48 -0400

    Update devoir.tf

--- devoir.tf
+++ devoir.tf
@@ -5,13 +5,71 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_instance" "InternalProd" {
+resource "google_compute_instance" "InternalProdUbuntu" {
   name         = "fermier"
   machine_type = "f1-micro"
+
 
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "Prodtraitement" {
+  name         = "cheval"
+  machine_type = "f1-micro"
+
+
+  boot_disk {
+    initialize_params {
+      image = "fedora-coreos-cloud/fedora-coreos-stable"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "prodDMZ" {
+  name         = "canard"
+  machine_type = "f1-micro"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance" "Prodinterne" {
+  name         = "mouton"
+  machine_type = "f1-micro"
+
+
+  boot_disk {
+    initialize_params {
+      image = "fedora-coreos-cloud/fedora-coreos-stable"
     }
   }
 
